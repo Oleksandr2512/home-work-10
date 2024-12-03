68bb889c90fda534e87889c8fe319bb8b8a8b6eb
Muranov <amuranov@luxoft.com>
2024-12-03 21:55:53 +0100

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int age = 13;
+        String name = "Oleksandr";
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+    }
+
+    // git init
+    // git add README.md
+    // git commit -m "first commit"
+    // git branch -M main
+    // git remote add origin https://github.com/Aviory/task4.git
+    // git push -u origin main
+}
